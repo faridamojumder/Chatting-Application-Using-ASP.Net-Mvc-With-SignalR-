a63770ab1922a7a5f952a11a9c503dd334198f35
faridamojumder <91402790+faridamojumder@users.noreply.github.com>
2022-09-28 17:04:52 +0600

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRExam
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message, string path)
+        {
+            Clients.All.addNewMessageToPage(name, message, path);
+        }
+    }
+}
