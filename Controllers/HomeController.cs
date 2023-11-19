836b8ebd81cbfb4cfd28c0a55da5fe80465d6774
faridamojumder <112897921+murad-hassan@users.noreply.github.com>
2023-11-19 16:31:21 +0600

    Update Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        public ActionResult UploadFile()
+        {
+            object result = null;
+            var files = Request.Files;
+            if (files != null && files.Count > 0)
+            {
+                var file = files[0];
+                var filePath = Server.MapPath("~/MyFiles/" + file.FileName);
+                file.SaveAs(filePath);
+                result = new { message = "file saved successfully." };
+            }
+            return Json(result);
+        }
+    }
+}
